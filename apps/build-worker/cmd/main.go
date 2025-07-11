30982beede2f3dcbce84417bdd8eae0fb2b74991
vky5 <vky05@proton.me>
2025-07-11 16:24:27 +0530

    feature: login docker to ecr, tag image & push

--- apps/build-worker/cmd/main.go
+++ apps/build-worker/cmd/main.go
@@ -1,8 +1,18 @@
 // entry point of worker 
 package main
 
+import (
+	"log"
+	"build-worker/internal/utils"
+)
 
 
 func main(){
-
+	log.Println("🔄 Starting build worker")
+	
+	// Load env variables
+	if err := utils.EnvInit("./.env"); err != nil {
+		log.Fatalf("❌ Failed to load env variables: %v", err)
+	}
+	
 }
