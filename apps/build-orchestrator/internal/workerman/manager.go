b62c5709fbaacc2eb2ef2792f9acde3e980f4690
vky5 <vky05@proton.me>
2025-07-18 19:23:46 +0530

    Implemented round robin logic for traffic distribution

--- apps/build-orchestrator/internal/workerman/manager.go
+++ apps/build-orchestrator/internal/workerman/manager.go
@@ -1,4 +1,7 @@
-//
+// workman stands for workermanger
+// this file handles the array of workers info and registering and deregistering them...
+
+// and also periodically running the scheduler to check for the status of the workers
 
 package workerman
 
@@ -8,15 +11,29 @@ import (
 	"time"
 )
 
-
 type Worker struct {
-	Info       *workerpb.WorkerInfo
-	LastSeen   time.Time
-	IsAlive    bool
+	Info     *workerpb.WorkerInfo
+	LastSeen time.Time
+	state    string // state can be busy, free, dead
 }
 
-// using maps for fastest lookups for WorkerId
-var (
-	mu sync.Mutex
-	workers = make(map[string]*Worker)
-)
+// keeping the array of all workers 
+type WorkerManager struct {
+	mu             sync.Mutex // so that when a type (example a varible w Workermanager, w.Worker is updated no other goroutine intefres with it)
+	workers        []*Worker
+	roundRobinIdx  int
+}
+
+
+// for safely updating the state of the worker
+func (wm *WorkerManager) SetWorkerState(workerID string, state string) {
+	wm.mu.Lock()
+	defer wm.mu.Unlock()
+
+	for _, w:=range wm.workers {
+		if w.Info.Id == workerID {
+			w.state = state
+			break
+		}
+	}
+}
